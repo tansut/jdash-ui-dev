141d86321955bb3d20256cd7d05759df9708d2dc
theutku <turkoglu.utku@gmail.com>
2017-06-13 15:15:25 +0300

    bs further additions

--- demoapp/demos/bs/index.js
+++ demoapp/demos/bs/index.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
 
-    demo.initChartist();
-
     $.notify({
         icon: 'pe-7s-gift',
         message: "Welcome to <b>JDash Bootstrap Dashboard</b>!"
 
     }, {
             type: 'info',
-            timer: 4000
+            timer: 2000
         });
 
 });
