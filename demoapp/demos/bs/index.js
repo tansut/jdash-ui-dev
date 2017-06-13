cc583215c1b6d15fc57de6504518bfb52db8038e
theutku <turkoglu.utku@gmail.com>
2017-06-13 11:19:11 +0300

    bs

---
+++ demoapp/demos/bs/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    demo.initChartist();
+
+    $.notify({
+        icon: 'pe-7s-gift',
+        message: "Welcome to <b>JDash Bootstrap Dashboard</b>!"
+
+    }, {
+            type: 'info',
+            timer: 4000
+        });
+
+});
