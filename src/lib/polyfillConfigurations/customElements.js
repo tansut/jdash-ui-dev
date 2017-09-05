6c41e8bbb3603c418949b24875a257d406d5b04d
theutku <turkoglu.utku@gmail.com>
2017-09-05 12:04:05 +0300

    .

--- src/lib/polyfillConfigurations/customElements.js
+++ src/lib/polyfillConfigurations/customElements.js
@@ -1,15 +1,21 @@
 (function () {
     var script = document.currentScript;
 
-    var isSafari = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    // var isSafari = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-    if (isSafari) {
-        var safariNative = (script.hasAttribute("data-safari-native") && script.getAttribute("data-safari-native")) || "false";
-        if (safariNative.toLowerCase() == "true" || safariNative == "1") {
-            window.customElements.forcePolyfill = false;
-        } else {
-            window.customElements.forcePolyfill = true;
-        }
-    }
-
+    // if (isSafari) {
+    //     var safariNative = (script.hasAttribute("data-safari-native") && script.getAttribute("data-safari-native")) || "false";
+    //     if (safariNative.toLowerCase() == "true" || safariNative == "1") {
+    //         window.customElements.forcePolyfill = false;
+    //     } else {
+    //         window.customElements.forcePolyfill = true;
+    //     }
+    // }
+    window.customElements.forcePolyfill = true;
+    // var viewMode = getCookie("view-mode");
+    // if (viewMode == "desktop") {
+    //     viewport.setAttribute('content', 'width=1024');
+    // } else if (viewMode == "mobile") {
+    //     viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+    // }
 })();
