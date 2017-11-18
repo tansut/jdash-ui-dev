cdc39f1909fa419b96a5a4e6568532aa3e8dd141
volkan.akbayir <volkan.akbayir@kalitte.com.tr>
2017-11-18 16:28:08 +0300

    remove polyfill support

--- src/lib/polyfillConfigurations/customElements.js
+++ src/lib/polyfillConfigurations/customElements.js
@@ -1,24 +1,22 @@
-(function () {
-    var script = document.currentScript;
+(function () {    
+    (function (arr) {
+        arr.forEach(function (item) {
+            if (item.hasOwnProperty('remove')) {
+                return;
+            }
+            Object.defineProperty(item, 'remove', {
+                configurable: true,
+                enumerable: true,
+                writable: true,
+                value: function remove() {
+                    this.parentNode.removeChild(this);
+                }
+            });
+        });
+    })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 
-    // var isSafari = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-    // if (isSafari) {
-    //     var safariNative = (script.hasAttribute("data-safari-native") && script.getAttribute("data-safari-native")) || "false";
-    //     if (safariNative.toLowerCase() == "true" || safariNative == "1") {
-    //         window.customElements.forcePolyfill = false;
-    //     } else {
-    //         window.customElements.forcePolyfill = true;
-    //     }
-    // }
     if(!("customElements" in window)){
         window.customElements = {};
     }
     window.customElements.forcePolyfill = true;
-    // var viewMode = getCookie("view-mode");
-    // if (viewMode == "desktop") {
-    //     viewport.setAttribute('content', 'width=1024');
-    // } else if (viewMode == "mobile") {
-    //     viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
-    // }
 })();
